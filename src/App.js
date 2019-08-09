6bb65408e3a2e31ed16c24f6c178a571d640c44f
rishabh dubey <rishabh@synsoftglobal.com>
2019-08-09 14:04:02 +0530

    commite by rishabh..

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import home from './home';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload & restart.
+          Edit <code>src/App.js</code> and save to reload & restart. This is change by rishabh
         </p>
         <a
           className="App-link"
