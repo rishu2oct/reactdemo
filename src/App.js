340472a0b62a638e4f68021985e4802ca287d12f
git <rishabh@synsoftglobal.com>
2019-08-14 15:34:40 +0530

    by rishabh

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // test file
+  // test file hello 
   return (
     <div className="App">
       <header className="App-header">
